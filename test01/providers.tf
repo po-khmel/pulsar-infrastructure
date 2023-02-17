977649786117715864e0f49ade707cc8197a60cc
po.khmel <khmelevskayapv@gmail.com>
2023-02-17 14:32:32 +0100

    openstack ver

--- test01/providers.tf
+++ test01/providers.tf
@@ -4,7 +4,7 @@ required_version = ">= 1.3.9"
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
-      version = "~> 1.22.0"
+      version = "~> 1.48.0"
     }
   }
 }
