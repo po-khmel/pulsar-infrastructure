25c3eb6b1f44eb407b98b265f2d5cf949416224f
po.khmel <khmelevskayapv@gmail.com>
2023-02-17 12:17:31 +0100

    firewalld

--- test01/nfs.tf
+++ test01/nfs.tf
@@ -51,29 +51,31 @@ data "template_cloudinit_config" "nfs-share" {
   gzip          = true
   base64_encode = true
 
-  # part {
-  #   content_type = "text/x-shellscript"
-  #   content      = "${file("${path.module}/files/create_share.sh")}"
-  # }
-
   part {
-    content_type = "text/cloud-config"
-    content      = <<-EOF
-    #cloud-config
-    write_files:
-    - content: |
-        /data/share *(rw,sync)
-      owner: root:root
-      path: /etc/exports
-      permissions: '0644'
-    runcmd:
-     - [ systemctl, enable, nfs-server ]
-     - [ systemctl, start, nfs-server ]
-     - [ exportfs, -avr ]
-    # ssh_authorized_keys:
-    #   - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC49BanKDSkoT22TWvNeL+4x/qcRi0a7Nuf+GmDXEEaCWlhvD7oeYoqVm/Jbbxo0FSDENwpMds5nR8MrdInOL1Ycp9sOoOsi0Sf1mMKhErHE2O+SHmQrPiKphams3wNSllKV80171E+7+ljYcUPREybBomZgYWlqeh46q+41AEFWxn6MYlQud/pa7TTnu/1egaWhX5W+P3l9Mo+x13LOywqbTl+545gvKg2bAHdkFkj/k/YKqM/DSFXT4Cx2r/OWZuR6oBLvsjmsld6rUdDhgIKqxQgK523NJv2gm0TS2JBXzFLsnH+ByIF55r1VCQlhYqfbl0w1O6exbc7pUnRy+ch
-  EOF
+    content_type = "text/x-shellscript"
+    content      = "${file("${path.module}/files/create_share.sh")}"
   }
+
+  # part {
+  #   content_type = "text/cloud-config"
+  #   content      = <<-EOF
+  #   #cloud-config
+  #   write_files:
+  #   - content: |
+  #       /data/share *(rw,sync)
+  #     owner: root:root
+  #     path: /etc/exports
+  #     permissions: '0644'
+  #   runcmd:
+  #     - firewall-cmd --permanent --add-service=nfs
+  #     - firewall-cmd --permanent --add-service=mountd
+  #     - firewall-cmd --permanent --add-service=rpc-bind
+  #     - firewall-cmd --reload
+  #    - [ systemctl, enable, nfs-server ]
+  #    - [ systemctl, start, nfs-server ]
+  #    - [ exportfs, -avr ]
+  # EOF
+  # }
 
 
 }
