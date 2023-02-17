388091994db0381ec24764b564d7ac0ab2e2af01
po.khmel <khmelevskayapv@gmail.com>
2023-02-17 11:30:38 +0100

    nfs with ssh

--- test01/nfs.tf
+++ test01/nfs.tf
@@ -26,19 +26,19 @@ resource "openstack_compute_instance_v2" "nfs-server" {
     delete_on_termination = true
   }
 
-  # user_data = "${data.template_cloudinit_config.nfs-share.rendered}"
-  user_data = <<-EOF
-    #cloud-config
-    write_files:
-    - content: |
-        /data           /etc/auto.data          nfsvers=3
-      owner: root:root
-      path: /etc/auto.master.d/data.autofs
-      permissions: '0644'
-    ssh_authorized_keys:
-      - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC49BanKDSkoT22TWvNeL+4x/qcRi0a7Nuf+GmDXEEaCWlhvD7oeYoqVm/Jbbxo0FSDENwpMds5nR8MrdInOL1Ycp9sOoOsi0Sf1mMKhErHE2O+SHmQrPiKphams3wNSllKV80171E+7+ljYcUPREybBomZgYWlqeh46q+41AEFWxn6MYlQud/pa7TTnu/1egaWhX5W+P3l9Mo+x13LOywqbTl+545gvKg2bAHdkFkj/k/YKqM/DSFXT4Cx2r/OWZuR6oBLvsjmsld6rUdDhgIKqxQgK523NJv2gm0TS2JBXzFLsnH+ByIF55r1VCQlhYqfbl0w1O6exbc7pUnRy+ch
-  EOF
-}
+  user_data = "${data.template_cloudinit_config.nfs-share.rendered}"
+  # user_data = <<-EOF
+  #   #cloud-config
+  #   write_files:
+  #   - content: |
+  #       /data           /etc/auto.data          nfsvers=3
+  #     owner: root:root
+  #     path: /etc/auto.master.d/data.autofs
+  #     permissions: '0644'
+  #   ssh_authorized_keys:
+  #     - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC49BanKDSkoT22TWvNeL+4x/qcRi0a7Nuf+GmDXEEaCWlhvD7oeYoqVm/Jbbxo0FSDENwpMds5nR8MrdInOL1Ycp9sOoOsi0Sf1mMKhErHE2O+SHmQrPiKphams3wNSllKV80171E+7+ljYcUPREybBomZgYWlqeh46q+41AEFWxn6MYlQud/pa7TTnu/1egaWhX5W+P3l9Mo+x13LOywqbTl+545gvKg2bAHdkFkj/k/YKqM/DSFXT4Cx2r/OWZuR6oBLvsjmsld6rUdDhgIKqxQgK523NJv2gm0TS2JBXzFLsnH+ByIF55r1VCQlhYqfbl0w1O6exbc7pUnRy+ch
+  # EOF
+# }
 
 
 
@@ -47,33 +47,34 @@ resource "openstack_blockstorage_volume_v2" "volume_nfs_data" {
   size = "${var.nfs_disk_size}"
 }
 
-# data "template_cloudinit_config" "nfs-share" {
-#   gzip          = true
-#   base64_encode = true
+data "template_cloudinit_config" "nfs-share" {
+  gzip          = true
+  base64_encode = true
 
-#   part {
-#     content_type = "text/x-shellscript"
-#     content      = "${file("${path.module}/files/create_share.sh")}"
-#   }
+  part {
+    content_type = "text/x-shellscript"
+    content      = "${file("${path.module}/files/create_share.sh")}"
+  }
 
-#   part {
-#     content_type = "text/cloud-config"
-#     content      = <<-EOF
-#     #cloud-config
-#     write_files:
-#     - content: |
-#         /data/share *(rw,sync)
-#       owner: root:root
-#       path: /etc/exports
-#       permissions: '0644'
+  part {
+    content_type = "text/cloud-config"
+    content      = <<-EOF
+    #cloud-config
+    write_files:
+    - content: |
+        /data/share *(rw,sync)
+      owner: root:root
+      path: /etc/exports
+      permissions: '0644'
 
-#     runcmd:
-#      - [ systemctl, enable, nfs-server ]
-#      - [ systemctl, start, nfs-server ]
-#      - [ exportfs, -avr ]
+    runcmd:
+     - [ systemctl, enable, nfs-server ]
+     - [ systemctl, start, nfs-server ]
+     - [ exportfs, -avr ]
 
-#   EOF
-#   }
-
-
-# }
+    ssh_authorized_keys:
+     - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC49BanKDSkoT22TWvNeL+4x/qcRi0a7Nuf+GmDXEEaCWlhvD7oeYoqVm/Jbbxo0FSDENwpMds5nR8MrdInOL1Ycp9sOoOsi0Sf1mMKhErHE2O+SHmQrPiKphams3wNSllKV80171E+7+ljYcUPREybBomZgYWlqeh46q+41AEFWxn6MYlQud/pa7TTnu/1egaWhX5W+P3l9Mo+x13LOywqbTl+545gvKg2bAHdkFkj/k/YKqM/DSFXT4Cx2r/OWZuR6oBLvsjmsld6rUdDhgIKqxQgK523NJv2gm0TS2JBXzFLsnH+ByIF55r1VCQlhYqfbl0w1O6exbc7pUnRy+ch
+  
+    EOF
+  }
+}
