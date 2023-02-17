821b1d51f83d862f24d2a345b59b8d25a11d1b6c
po.khmel <khmelevskayapv@gmail.com>
2023-02-17 14:31:40 +0100

    new tf

--- test01/providers.tf
+++ test01/providers.tf
@@ -1,1 +1,10 @@
-provider "openstack" {version = "1.22.0"}
+# provider "openstack" {version = "1.22.0"}
+terraform {
+required_version = ">= 1.3.9"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.22.0"
+    }
+  }
+}
