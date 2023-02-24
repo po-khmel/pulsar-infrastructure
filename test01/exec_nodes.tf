12d4105dbfebb24de0d082dfad9421a4ef7c2f2b
po.khmel <khmelevskayapv@gmail.com>
2023-02-24 13:35:04 +0100

    --no-dry-run

--- test01/exec_nodes.tf
+++ test01/exec_nodes.tf
@@ -57,7 +57,7 @@ resource "openstack_compute_instance_v2" "exec-node" {
     - [sh, -xc, sed -i 's|nameserver 10.0.2.3||g' /etc/resolv.conf]
     - [sh, -xc, sed -i 's|localhost.localdomain|$(hostname -f)|g' /etc/telegraf/telegraf.conf]
     - [systemctl, restart, telegraf]
-    - curl -fsSL https://get.htcondor.org | /bin/bash -s -- --execute vgcn-central-manager-usegalaxy-eu
+    - curl -fsSL https://get.htcondor.org | /bin/bash -s -- --no-dry-run --execute vgcn-central-manager-usegalaxy-eu
     - sudo condor_config_val use security:get_htcondor_idtokens
   EOF
 }
