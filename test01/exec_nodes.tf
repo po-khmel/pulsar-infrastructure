aaaed302890082d20ab5c6000c46345c21390bc1
po.khmel <khmelevskayapv@gmail.com>
2023-02-27 08:24:44 +0100

    no condor conf

--- test01/exec_nodes.tf
+++ test01/exec_nodes.tf
@@ -14,32 +14,32 @@ resource "openstack_compute_instance_v2" "exec-node" {
   user_data = <<-EOF
     #cloud-config
     write_files:
-    - content: |
-        CONDOR_HOST = ${openstack_compute_instance_v2.central-manager.network.0.fixed_ip_v4}
-        ALLOW_WRITE = *
-        ALLOW_READ = $(ALLOW_WRITE)
-        ALLOW_ADMINISTRATOR = *
-        ALLOW_NEGOTIATOR = $(ALLOW_ADMINISTRATOR)
-        ALLOW_CONFIG = $(ALLOW_ADMINISTRATOR)
-        ALLOW_DAEMON = $(ALLOW_ADMINISTRATOR)
-        ALLOW_OWNER = $(ALLOW_ADMINISTRATOR)
-        ALLOW_CLIENT = *
-        DAEMON_LIST = MASTER, SCHEDD, STARTD
-        FILESYSTEM_DOMAIN = vgcn
-        UID_DOMAIN = vgcn
-        TRUST_UID_DOMAIN = True
-        SOFT_UID_DOMAIN = True
-        # run with partitionable slots
-        CLAIM_PARTITIONABLE_LEFTOVERS = True
-        NUM_SLOTS = 1
-        NUM_SLOTS_TYPE_1 = 1
-        SLOT_TYPE_1 = 100%
-        SLOT_TYPE_1_PARTITIONABLE = True
-        ALLOW_PSLOT_PREEMPTION = False
-        STARTD.PROPORTIONAL_SWAP_ASSIGNMENT = True
-      owner: root:root
-      path: /etc/condor/condor_config.local
-      permissions: '0644'
+    # - content: |
+    #     CONDOR_HOST = ${openstack_compute_instance_v2.central-manager.network.0.fixed_ip_v4}
+    #     ALLOW_WRITE = *
+    #     ALLOW_READ = $(ALLOW_WRITE)
+    #     ALLOW_ADMINISTRATOR = *
+    #     ALLOW_NEGOTIATOR = $(ALLOW_ADMINISTRATOR)
+    #     ALLOW_CONFIG = $(ALLOW_ADMINISTRATOR)
+    #     ALLOW_DAEMON = $(ALLOW_ADMINISTRATOR)
+    #     ALLOW_OWNER = $(ALLOW_ADMINISTRATOR)
+    #     ALLOW_CLIENT = *
+    #     DAEMON_LIST = MASTER, SCHEDD, STARTD
+    #     FILESYSTEM_DOMAIN = vgcn
+    #     UID_DOMAIN = vgcn
+    #     TRUST_UID_DOMAIN = True
+    #     SOFT_UID_DOMAIN = True
+    #     # run with partitionable slots
+    #     CLAIM_PARTITIONABLE_LEFTOVERS = True
+    #     NUM_SLOTS = 1
+    #     NUM_SLOTS_TYPE_1 = 1
+    #     SLOT_TYPE_1 = 100%
+    #     SLOT_TYPE_1_PARTITIONABLE = True
+    #     ALLOW_PSLOT_PREEMPTION = False
+    #     STARTD.PROPORTIONAL_SWAP_ASSIGNMENT = True
+    #   owner: root:root
+    #   path: /etc/condor/condor_config.local
+    #   permissions: '0644'
     - content: |
         /data           /etc/auto.data          nfsvers=3
       owner: root:root
