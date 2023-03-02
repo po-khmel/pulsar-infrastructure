70e61ec866b32ca1102d3de980a6b94ab541c03c
po.khmel <khmelevskayapv@gmail.com>
2023-03-02 14:24:08 +0100

    typo

--- test01/exec_nodes.tf
+++ test01/exec_nodes.tf
@@ -83,13 +83,13 @@ resource "openstack_compute_instance_v2" "exec-node" {
     # ssh_authorized_keys:
     #   - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC49BanKDSkoT22TWvNeL+4x/qcRi0a7Nuf+GmDXEEaCWlhvD7oeYoqVm/Jbbxo0FSDENwpMds5nR8MrdInOL1Ycp9sOoOsi0Sf1mMKhErHE2O+SHmQrPiKphams3wNSllKV80171E+7+ljYcUPREybBomZgYWlqeh46q+41AEFWxn6MYlQud/pa7TTnu/1egaWhX5W+P3l9Mo+x13LOywqbTl+545gvKg2bAHdkFkj/k/YKqM/DSFXT4Cx2r/OWZuR6oBLvsjmsld6rUdDhgIKqxQgK523NJv2gm0TS2JBXzFLsnH+ByIF55r1VCQlhYqfbl0w1O6exbc7pUnRy+ch
     runcmd:
+    - python3 -m pip install ansible
+    - ansible-galaxy install -p /home/centos/.ansible/roles usegalaxy_eu.htcondor
+    - ansible-playbook -i localhost /home/centos/condor.yml
+    - systemctl start condor
     - [sh, -xc, sed -i 's|nameserver 10.0.2.3||g' /etc/resolv.conf]
     - [sh, -xc, sed -i 's|localhost.localdomain|$(hostname -f)|g' /etc/telegraf/telegraf.conf]
     - [systemctl, restart, telegraf]
-    - [python3, -m, pip, install, ansible]
-    - [ansible-galaxy, install, -p, /home/centos/.ansible/roles, usegalaxy_eu.htcondor]
-    - [ansible-playbook, -i, 'localhost,', /home/centos/condor.yml]
-    - systemctl start condor
     # - curl -fsSL https://get.htcondor.org | sudo GET_HTCONDOR_PASSWORD=demo /bin/bash -s -- --no-dry-run --execute ${openstack_compute_instance_v2.central-manager.network.0.fixed_ip_v4}
   EOF
 }
